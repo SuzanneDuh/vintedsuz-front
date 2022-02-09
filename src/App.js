3a5837613e58a20c4781e86eb7fc50ec224eafd3
Suzanne DUHAMEL <suzanneduhamel@MacBook-Pro-de-Suzanne-2.local>
2022-02-09 17:45:08 +0100

    MercV2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return <h1>helllo suzon</h1>;
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      // ?? console.log(response.data);
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+  return isLoading ? <h1>Patieennnnnnnnnnnnce</h1> : <h1>helllo suzon</h1>;
 }
 
 export default App;
